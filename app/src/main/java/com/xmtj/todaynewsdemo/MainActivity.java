5314c25c46025f1189bf0bee3181bf0133ade3b3
snoopymkkl <37484139@qq.com>
2017-03-21 15:46:45 +0800

    Tinker测试成功

--- app/src/main/java/com/xmtj/todaynewsdemo/MainActivity.java
+++ app/src/main/java/com/xmtj/todaynewsdemo/MainActivity.java
@@ -1,13 +1,31 @@
 package com.xmtj.todaynewsdemo;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.btn_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //点击安装更新包
+                TinkerInstaller.onReceiveUpgradePatch
+                        (getApplicationContext(),
+                                Environment.getExternalStorageDirectory().getAbsolutePath()
+                                        + "/patch_signed_7zip.apk");
+
+            }
+        });
+
     }
 }
